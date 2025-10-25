36a3232ccd53c9a348a9e5a10bbb82b6b31c4094
ljquanlab <ljquan@suda.edu.cn>
2025-10-25 23:01:06 +0800

    Update GCAST.py

--- GCAST/GCAST.py
+++ GCAST/GCAST.py
@@ -50,13 +50,11 @@ class GCAST:
         self.norm_value = graph_dict["norm_value"]
 
         self.input_dim = self.X.shape[1]
-        self.constrative_loss = InfoNCEWithAdj(temperature=self.temperature, normalize=False)   #  ###
+        self.constrative_loss = InfoNCEWithAdj(temperature=self.temperature, normalize=False) 
         self.model = Module(self.input_dim,
                     feat_hidden1=feat_hidden1,feat_hidden2=feat_hidden2,
                     gcn_hidden1=gcn_hidden1,gcn_hidden2=gcn_hidden2,).to(self.device).float()
 
-        # self.early_stopping1 =  EarlyStopping(patience=50, verbose=False, mode="return")
-        # self.early_stopping2 =  EarlyStopping(patience=50, verbose=False, mode="return")
         self.model_path1  = f"{model_name}_Phase1.pth"
         self.model_path2 = f"{model_name}_Phase2.pth"
         self.early_stopping1 = EarlyStopping(patience=50, verbose=False, path= self.model_path1, mode='save')
@@ -92,20 +90,11 @@ class GCAST:
         self.model.train()
 
         for _ in tqdm(range(epochs)):
-            # if i % 5 == 0:
-            #     print("---torch.cuda.get_rng_state_all:",torch.cuda.get_rng_state_all())
-
-            # print("Before rand:", torch.get_rng_state()[:10])
-            # x = torch.rand(1)  
-            # print("After rand:", torch.get_rng_state()[:10])
-
             self.optimizer.zero_grad()
-
 
             X1, adj1 = self.graph_augment(self.X, self.adj_norm, drop_edge_rate, drop_node_rate)
             X2, adj2 = self.graph_augment(self.X, self.adj_norm, 0, 0)
 
-            # 得到两个视图的编码
             z1, mu1, logvar1, de_feat1, = self.model(X1, adj1)
             z2, mu2, logvar2, de_feat2, = self.model(X2, adj2)
 
@@ -153,19 +142,10 @@ class GCAST:
             torch.cuda.set_rng_state_all(checkpoint['cuda_rng_states'])
   
     def eval_model(self):
-        # checkpoint = torch.load(self.model_path, map_location=self.device)
-        # self.model.load_state_dict(checkpoint['model_state_dict'])  # 返回值不要接链式调用
         self.model.to(self.device).float().eval()
-        # self.model.eval()  #  将模型切换到评估模式，evaluation training
-        # torch.set_rng_state(checkpoint['rng_state'])
-        # if checkpoint['cuda_rng_states'] is not None and torch.cuda.is_available():
-        #     torch.cuda.set_rng_state_all(checkpoint['cuda_rng_states'])
         with torch.no_grad():
             latent_z, mu, logvar, de_feat = self.model(self.X, self.adj_norm)
             q = self.model.dec(latent_z)
-
-            # from sklearn.preprocessing import StandardScaler
-            # out = StandardScaler().fit_transform(de_feat)
 
             latent_z = latent_z.detach().cpu().numpy()
             q = q.detach().cpu().numpy()
@@ -186,7 +166,6 @@ class GCAST:
             # DEC clustering update
             if epoch_id % dec_interval == 0:
                 _,_, tmp_q  = self.eval_model()
-                # tmp_p = target_distribution(torch.tensor(tmp_q, dtype=torch.float64, device=self.device)).float()
                 tmp_p = target_distribution(torch.tensor(tmp_q, device=self.device)).float()
                 self.model.train()
 
